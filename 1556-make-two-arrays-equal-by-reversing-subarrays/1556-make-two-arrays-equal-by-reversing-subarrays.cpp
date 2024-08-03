824a60caf5a0fc20e45d287a081b459715fae2a1
Vijay Kumar <97971494+VIJAYKUMAR2435@users.noreply.github.com>
2024-08-03 12:52:44 +0530

    Time: 13 ms (29.25%), Space: 18.3 MB (29.59%) - LeetHub

---
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+       vector<int>cnt1(1001);
+       vector<int>cnt2(1001);
+       for(auto &v:target){
+        cnt1[v]++;
+       }
+       for(auto &v:arr){
+        cnt2[v]++;
+       }
+       return cnt1==cnt2;
+        
+    }
+};
