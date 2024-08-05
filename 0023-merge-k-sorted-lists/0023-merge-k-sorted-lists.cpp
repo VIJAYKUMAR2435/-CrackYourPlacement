b89765f2ee82299697d3061f7a29aa66fe615feb
Vijay Kumar <97971494+VIJAYKUMAR2435@users.noreply.github.com>
2024-08-06 00:25:47 +0530

    Time: 170 ms (9.37%), Space: 18.5 MB (23.58%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,23 +10,32 @@
  */
 class Solution {
 public:
-    ListNode* merge(ListNode* a,ListNode* b){
-        if(!a)return b;
-        if(!b)return a;
-        if(a->val <= b->val){
-            a->next = merge(a->next,b);
-            return a;
-        }else{
-            b->next = merge(a,b->next);
-            return b;
-        }
-        return NULL;
+ListNode*merge(ListNode*a,ListNode*b){
+    if(!a){
+        return b;
     }
+    if(!b){
+        return a;
+    }
+    if(a->val<=b->val){
+        a->next=merge(a->next,b);
+        return a;
+    }
+    else{
+        b->next=merge(a,b->next);
+        return b;
+    }
+    return NULL;
+}
     ListNode* mergeKLists(vector<ListNode*>& v) {
-        if(v.size()==0)return NULL;
-        else if(v.size()==1)return v[0]; 
-        for(int i = 1; i < v.size(); i++){
-            v[0] = merge(v[0],v[i]);
+        if(v.size()==0){
+            return 0;
+        }
+        if(v.size()==1){
+            return v[0];
+        }
+        for(int i=1;i<v.size();i++){
+            v[0]=merge(v[0],v[i]);
         }
         return v[0];
     }
