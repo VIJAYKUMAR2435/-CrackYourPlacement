86876f92d22613aa0ebc04f81c3465737cbe7e5a
Vijay Kumar <97971494+VIJAYKUMAR2435@users.noreply.github.com>
2024-08-12 08:39:44 +0530

    Time: 31 ms (22.46%), Space: 24.9 MB (6.31%) - LeetHub

--- 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
+++ 0789-kth-largest-element-in-a-stream/0789-kth-largest-element-in-a-stream.cpp
@@ -5,12 +5,9 @@ class KthLargest {
 public:
     KthLargest(int k, vector<int>& nums) {
            k_ = k;
-        // Sort in descending order. The reason this is needed is if nums.size() > k, we need to
-        // ensure k largest elements will be stored in the heap_ when call next for loop
+      
         sort(nums.begin(), nums.end(), greater<int>()); 
-        // The reason i < min(k, static_cast<int>(nums.size())) is needed, is to cover all edge case:
-        // if nums.size() > k, then k largest elemet will be pushed to the heap_
-        // if nums.size() <= k, then all elements in nums will be pushed to heap_
+   
         for (int i = 0; i < min(k, static_cast<int>(nums.size())); ++i) heap_.push(nums[i]);
     }
     
